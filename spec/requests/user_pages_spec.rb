c6f7feb8dd93e44347a7463d0209dfa4782e32f6
rallapalla <github@janishofmann.com>
2013-11-26 16:27:06 +0100

    Finish User Sign in and out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,7 +34,15 @@ describe "User pages" do
       end
       it "should create user" do
         expect { click_button submit}.to change(User, :count).by(1)
-      end    
+      end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'example@service.com') }
+        
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
     
     describe "with invalid information" do
