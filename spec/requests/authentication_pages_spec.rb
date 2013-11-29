3c3fb625a37f7f7a96006e0c34c39308b93cfa5e
rallapalla <github@janishofmann.com>
2013-11-29 16:38:15 +0100

    add user-index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,6 +15,7 @@ describe "Authentication" do
       before { sign_in(user) }
       
       it { should have_title( user.name ) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should have_link('Settings', href: edit_user_path(user)) }
@@ -62,14 +63,22 @@ describe "Authentication" do
       end
       
       describe "in the Users controller" do
+        
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_title('Sign in') }
         end
+        
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+        
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+        
       end
     end
     
