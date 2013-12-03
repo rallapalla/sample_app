810ddd25a09a7aaeeb795e986e7691ae299bed86
rallapalla <github@janishofmann.com>
2013-12-03 11:13:00 +0100

    finish user pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name      "Janis Hofmann"
-    email     "janishofmann@me.com"
+    sequence(:name) { |n| "Person_#{n}"}
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password  "foobar"
     password_confirmation "foobar"
   end
